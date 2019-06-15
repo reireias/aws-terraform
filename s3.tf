0227537573750acd8d69d99e822a6779ac955ab6
reireias <reireias@gmail.com>
2019-06-15 16:56:45 +0900

    update s3 policy

--- s3.tf
+++ s3.tf
@@ -12,31 +12,42 @@ resource "aws_s3_bucket_policy" "billing" {
 
   policy = <<POLICY
 {
-    "Version": "2008-10-17",
-    "Id": "Policy1335892530063",
-    "Statement": [
-        {
-            "Sid": "Stmt1335892150622",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "billingreports.amazonaws.com"
-            },
-            "Action": [
-                "s3:GetBucketAcl",
-                "s3:GetBucketPolicy"
-            ],
-            "Resource": "arn:aws:s3:::reireias-billing"
-        },
-        {
-            "Sid": "Stmt1335892526596",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "billingreports.amazonaws.com"
-            },
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::reireias-billing/*"
-        }
-    ]
+  "Version": "2008-10-17",
+  "Id": "Policy1335892530063",
+  "Statement": [
+    {
+      "Sid": "Stmt1335892150622",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "billingreports.amazonaws.com"
+      },
+      "Action": [
+        "s3:GetBucketAcl",
+        "s3:GetBucketPolicy"
+      ],
+      "Resource": "arn:aws:s3:::reireias-billing"
+    },
+    {
+      "Sid": "Stmt1335892526596",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "billingreports.amazonaws.com"
+      },
+      "Action": "s3:PutObject",
+      "Resource": "arn:aws:s3:::reireias-billing/*"
+    },
+    {
+      "Sid": "BillingBucketUser",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::551198746745:user/billing-bucket"
+      },
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": "arn:aws:s3:::reireias-billing/*"
+    }
+  ]
 }
 POLICY
 }
