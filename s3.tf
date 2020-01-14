6d8397873afbe7220d3d6b02b3beb17005740454
reireias <reireias@gmail.com>
2020-01-14 21:49:49 +0900

    add factorio bucket

--- s3.tf
+++ s3.tf
@@ -55,3 +55,35 @@ resource "aws_s3_bucket_policy" "billing" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "factorio" {
+  bucket = "factorio.timelapse"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    Name = "factorio.timelapse"
+  }
+}
+
+resource "aws_s3_bucket_policy" "factorio_policy" {
+  bucket = aws_s3_bucket.factorio.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": ["arn:aws:s3:::factorio.timelapse/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
