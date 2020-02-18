5271c01add1350014c3df8f8e8aefe58e3cc1838
reireias <reireias@gmail.com>
2020-02-18 23:25:39 +0900

    add billing bucket

--- s3.tf
+++ s3.tf
@@ -29,3 +29,50 @@ resource "aws_s3_bucket_policy" "factorio_policy" {
 }
 POLICY
 }
+
+resource "aws_s3_bucket" "billing" {
+  bucket = "reireias-aws-billing"
+  acl    = "private"
+
+  tags = {
+    Name = "reireias-aws-billing"
+  }
+}
+
+data "aws_iam_policy_document" "billing" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["billingreports.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:GetBucketAcl",
+      "s3:GetBucketPolicy",
+    ]
+
+    resources = [
+      aws_s3_bucket.billing.arn
+    ]
+  }
+
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["billingreports.amazonaws.com"]
+    }
+
+    actions = [
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.billing.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "billing" {
+  bucket = "${aws_s3_bucket.billing.id}"
+  policy = data.aws_iam_policy_document.billing.json
+}
