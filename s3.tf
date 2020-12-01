8077c172ead65db710663784a868da60168e66ac
reireias <reireias@gmail.com>
2020-12-01 23:11:14 +0900

    remove factorio bucket

--- s3.tf
+++ s3.tf
@@ -1,37 +1,3 @@
-resource "aws_s3_bucket" "factorio" {
-  bucket = "factorio.timelapse"
-  acl    = "public-read"
-
-  force_destroy = true
-
-  website {
-    index_document = "index.html"
-  }
-
-  tags = {
-    Name = "factorio.timelapse"
-  }
-}
-
-resource "aws_s3_bucket_policy" "factorio_policy" {
-  bucket = aws_s3_bucket.factorio.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect":"Allow",
-      "Principal": "*",
-      "Action": ["s3:GetObject"],
-      "Resource": ["arn:aws:s3:::factorio.timelapse/*"
-      ]
-    }
-  ]
-}
-POLICY
-}
-
 resource "aws_s3_bucket" "billing" {
   bucket = "reireias-aws-billing"
   acl    = "private"
