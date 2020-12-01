a449b195c7b1757d3a25cb896018250999dc0b87
reireias <reireias@gmail.com>
2020-12-01 23:34:35 +0900

    fix tfsec error

--- s3.tf
+++ s3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "billing" {
   bucket = "reireias-aws-billing"
   acl    = "private"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags = {
     Name = "reireias-aws-billing"
   }
