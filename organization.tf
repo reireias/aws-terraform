c2e2d23ed435db36cb0b10b5f90abc9e0e3ba4b4
reireias <reireias@gmail.com>
2020-12-06 00:34:58 +0900

    add Sub Account B

--- organization.tf
+++ organization.tf
@@ -4,3 +4,8 @@ resource "aws_organizations_account" "subaccount_a" {
   name  = "subaccount_a"
   email = "reireias+aws-sub-a@gmail.com"
 }
+
+resource "aws_organizations_account" "subaccount_b" {
+  name  = "subaccount_b"
+  email = "reireias+aws-sub-b@gmail.com"
+}
